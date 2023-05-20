bbf25901344473591087e18ef45a600367593b94
Octavio Martinez <omartinez1618@gmail.com>
2023-05-20 10:49:02 -0400

    clean code

--- services/ModulesService.js
+++ services/ModulesService.js
@@ -18,14 +18,3 @@ export default class ModulesService extends BaseService {
         }
     }
 }
-
-
-/**
- * {
-"access_token": "1000.c7ec4b346af15d7c4638920b98a6b13f.f76123319fdb19836e997bd9e0680971",
-"refresh_token": "1000.e4297df235ac6b2aaf0fe139b775d606.f16a0422fdc9114ac496172e8390ed93",
-"api_domain": "https://www.zohoapis.com",
-"token_type": "Bearer",
-"expires_in": 3600
-}
- */
